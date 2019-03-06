e13a10c25816796e8b7e9ecc6452b5c068a766b8
William Wang <wow7@cornell.edu>
2019-03-05 23:43:16 -0500

    implemented api backend

--- index.js
+++ index.js
@@ -1,20 +1,54 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const giveMeAJoke = require('give-me-a-joke');
+const randomName = require('random-name')
 
 const app = express();
+
 app.use(express.static('public'));
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
+let jokeCount = 0;
 
+app.get('/', (req, res, next) => {
+    res.sendFile('index.html');
+})
 
+app.post('/api/random-joke', (req, res, next) => {
+    jokeCount += 1;
+    if (req.body.firstName && req.body.lastName) {
+        const fn = req.body.firstName;
+        const ln = req.body.lastName;
+        giveMeAJoke.getCustomJoke(fn, ln, (joke) => {
+            res.send({ joke: joke });
+        });
+    } else {
+        // malformed POST request parameters
+        next(new Error('Malformed POST request'));
+    }
+});
+
+app.get('/api/random-name', (req, res) => {
+    res.send({ first: randomName.first(), last: randomName.last() });
+})
+
+app.get('/api/joke-count', (req, res) => {
+    res.send({ jokeCount: jokeCount });
+})
 
 // 404, no matching route found
-app.use(function (req, res, next) {
-    res.status(404).send("Invalid API route")
+app.use((req, res) => {
+    res.status(404).send("Invalid API route");
 })
+
+// route for handling errors
+app.use((err, req, res) => {
+    res.status(400).send(err);
+});
 
 const PORT = 3000;
 
 app.listen(PORT, () => {
-    console.log(`server running on port ${PORT}`)
+    console.log(`server running on port ${PORT}`);
 });
